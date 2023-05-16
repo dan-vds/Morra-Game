a5d89373749577bf9420ac83f1bf96e08adc5756
dan-vds <danielvds101@gmail.com>
2023-05-16 23:18:21 +1200

    Added a check to make sure sum chosen is between 1 and 10

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -5,26 +5,33 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Morra {
 
   private int roundnum = 1;
+  private String name;
 
   public Morra() {}
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    name = options[0];
   }
 
   public void play() {
     MessageCli.START_ROUND.printMessage(Integer.toString(roundnum));
     roundnum++;
     Boolean right = false;
+    String[] inputArray = new String[2];
     while (!right) {
       MessageCli.ASK_INPUT.printMessage();
       String input = Utils.scanner.nextLine();
-      String[] inputArray = input.split(" ");
+      inputArray = input.split(" ");
       if (inputArray.length != 2) {
         MessageCli.INVALID_INPUT.printMessage();
         continue;
       }
       if (Integer.parseInt(inputArray[0]) < 1 || Integer.parseInt(inputArray[0]) > 5) {
+        MessageCli.INVALID_INPUT.printMessage();
+        continue;
+      }
+      if (Integer.parseInt(inputArray[1]) < 1 || Integer.parseInt(inputArray[1]) > 10) {
         MessageCli.INVALID_INPUT.printMessage();
         continue;
       }
@@ -42,6 +49,7 @@ public class Morra {
       }
       right = true;
     }
+    MessageCli.PRINT_INFO_HAND.printMessage(name, inputArray[0], inputArray[1]);
   }
 
   public void playCheck() {}
