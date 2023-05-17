39bbf38f577bbdce3965ce222f4bc698062b091d
dan-vds <danielvds101@gmail.com>
2023-05-17 20:30:00 +1200

    added returning of the maximum played finger value for top strategy

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -12,16 +12,17 @@ public class TopStrategy implements Strategy {
   public int[] playHand(Morra morra) {
     previousFingers = morra.getPreviousFingers();
     int oneFinger = Collections.frequency(previousFingers, 1);
-    int twoFinger = Collections.frequency(previousFingers, 1);
-    int threeFinger = Collections.frequency(previousFingers, 1);
-    int fourFinger = Collections.frequency(previousFingers, 1);
-    int fiveFinger = Collections.frequency(previousFingers, 1);
-
+    int twoFinger = Collections.frequency(previousFingers, 2);
+    int threeFinger = Collections.frequency(previousFingers, 3);
+    int fourFinger = Collections.frequency(previousFingers, 4);
+    int fiveFinger = Collections.frequency(previousFingers, 5);
     List<Integer> fingersArray =
         Arrays.asList(oneFinger, twoFinger, threeFinger, fourFinger, fiveFinger);
-    int maxFingers = Collections.max(fingersArray);
-    System.out.println(maxFingers);
-    // int[] cPUHand = {fingers, sum};
-    return null;
+    int commonFingers = Collections.max(fingersArray);
+    int index = fingersArray.indexOf(commonFingers);
+    index += 1;
+    int fingers = Utils.getRandomNumber(1, 5);
+    int[] cPUHand = {fingers, fingers + index};
+    return cPUHand;
   }
 }
