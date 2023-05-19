574d7dfe9fdb6735acbcd59858db56953c4b30c4
dan-vds <danielvds101@gmail.com>
2023-05-19 13:27:35 +1200

    Added scoring counter and exit when game is complete

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -10,6 +10,9 @@ public class Morra {
   private Ai ai;
   private List<Integer> previousFingers = new ArrayList<>();
   private int roundnum = 1;
+  private int pointsToWin;
+  private int aiPoints = 0;
+  private int playerPoints = 0;
 
   public Morra() {}
 
@@ -17,9 +20,15 @@ public class Morra {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
     name = options[0];
     ai = AiFactory.createAi(difficulty, previousFingers);
+    roundnum = 1;
+    this.pointsToWin = pointsToWin;
   }
 
   public void play() {
+    if (this.ai == null) {
+      MessageCli.GAME_NOT_STARTED.printMessage();
+      return;
+    }
     MessageCli.START_ROUND.printMessage(Integer.toString(roundnum));
     int[] aiHand = ai.getHand();
     int aiFingers = aiHand[0];
@@ -33,21 +42,33 @@ public class Morra {
       right = true;
       MessageCli.ASK_INPUT.printMessage();
       String input = Utils.scanner.nextLine();
-      inputArray = input.split(" ");
-      if (!inputArray[0].isEmpty() && !inputArray[1].isEmpty()) {
-        fingers = inputArray[0];
-        sum = inputArray[1];
-      } else {
+      try {
+        inputArray = input.split(" ");
+      } catch (Exception e) {
         MessageCli.INVALID_INPUT.printMessage();
         right = false;
+        continue;
       }
-      if (Integer.parseInt(fingers) < 1
+      try {
+        if (!inputArray[0].isEmpty() && !inputArray[1].isEmpty()) {
+          fingers = inputArray[0];
+          sum = inputArray[1];
+        } else {
+          MessageCli.INVALID_INPUT.printMessage();
+          right = false;
+        }
+      } catch (Exception e) {
+        MessageCli.INVALID_INPUT.printMessage();
+        right = false;
+        continue;
+      }
+      if (!(Utils.isInteger(sum))
+          || !(Utils.isInteger(fingers))
+          || Integer.parseInt(fingers) < 1
           || Integer.parseInt(fingers) > 5
           || inputArray.length != 2
           || Integer.parseInt(sum) < 1
-          || Integer.parseInt(sum) > 10
-          || !(Utils.isInteger(sum))
-          || !(Utils.isInteger(fingers))) {
+          || Integer.parseInt(sum) > 10) {
         MessageCli.INVALID_INPUT.printMessage();
         right = false;
       }
@@ -61,23 +82,26 @@ public class Morra {
     int playerFingers = Integer.parseInt(inputArray[0]);
     if (playerFingers + aiFingers == playerSum && playerFingers + aiFingers != aiSum) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("HUMAN_WINS");
+      playerPoints++;
     } else if (playerFingers + aiFingers != playerSum && playerFingers + aiFingers == aiSum) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("AI_WINS");
+      aiPoints++;
     } else {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("DRAW");
+    }
+    if (playerPoints == pointsToWin) {
+      MessageCli.END_GAME.printMessage(name, Integer.toString(roundnum));
+      ai = null;
+      return;
+    } else if (aiPoints == pointsToWin) {
+      MessageCli.END_GAME.printMessage("Jarvis", Integer.toString(roundnum));
+      ai = null;
+      return;
     }
     roundnum++;
   }
 
-  // public int getRoundnum() {
-  //   return roundnum;
-  // }
-
   public void playCheck() {}
 
   public void showStats() {}
-
-  // public List<Integer> getPreviousFingers() {
-  //   return previousFingers;
-  // }
 }
